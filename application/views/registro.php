bd924ffaca30bbce24bc89dd1d7a28755b2f4ff2
JavierYanezSepulveda <javierandres.y.s@gmail.com>
2018-12-21 10:52:30 -0300

    1

--- application/views/registro.php
+++
@@ -1,6 +0,0 @@
-
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-     <link href="css/style.css" rel="stylesheet">
-<p>Registro Exitoso </p>
-
-<a href="<?php echo base_url();?>/productos" class="btn btn-danger"> volver</a>
--- application/views/view_sucess.php
+++
@@ -1,4 +0,0 @@
-<h1>¡¡registro Ingresado con exito!!</h1>
-
-
- <a href="<?php echo base_url();?>productos" class="btn btn-danger" >Volver</a>
